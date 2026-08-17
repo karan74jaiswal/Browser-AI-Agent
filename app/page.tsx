c1faf66a267e4e27de33327e45fe820a0119c8d3
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2026-08-18 03:24:52 +0530

    feat: Add Clerk Auth

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh p-6">
+    <div className="flex min-h-[calc(100vh-3.5rem)] p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
           <h1 className="font-medium">Project ready!</h1>
