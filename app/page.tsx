8a31bb0ed7838f86a72f36fa637f5a8bb7ef4233
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2026-08-18 08:22:28 +0530

    added .railwayignore instead of dockerignore

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-4 p-6">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-4 p-5">
       <UserButton />
       <OrganizationSwitcher
         afterCreateOrganizationUrl="/"
